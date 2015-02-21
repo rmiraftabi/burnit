151c0987aeb6841c4de149e8985b01aa043d6a8a
Ramin Miraftabi <rmiraftabi@gmail.com>
2015-02-21 11:43:38 +0200

    putting in some base work

---
+++ test/controllerTest.js
@@ -0,0 +1,26 @@
+var data = require('../lib/data.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.testCalculation = function(test) {
+  test.equal(data.METs.crossfit, 8.0);
+  test.done();
+}
